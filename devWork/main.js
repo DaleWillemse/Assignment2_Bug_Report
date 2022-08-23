124a6e339057fe13d82f226728da3f2a33b8dded
Devlyn Kempen <578616@student.belgiumcampus.ac.za>
2022-08-23 20:21:57 +0200

    My attempt for the layout without Bootstrap

--- devWork/main.js
+++ devWork/main.js
@@ -3,11 +3,27 @@ function cCrudClick(){
   alert("Create");
 }
 function rCrudClick(){
-  alert("Update");
+  alert("Read");
 }
 function uCrudClick(){
-  alert("Read");
+  alert("Update");
 }
 function dCrudClick(){
   alert("Delete");
+}
+
+function openProject() {
+  document.getElementById("createProject").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("createProject").style.display = "none";
+}
+function getInputFromTextBox() {
+    var input = document.getElementById("userInput").value;
+    alert(input);
+}
+
+function firstProject(){
+  document.getElementById("p1").innerHTML = "New text!";
 }
