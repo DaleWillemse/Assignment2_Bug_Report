6857a2d9e964b56b4f6157fc63f1f9e3cb08b6fe
DaleWillemse <DaleWillemse000@gmail.com>
2022-08-24 10:38:04 +0200

    unpacked zip and cleaned up files.

---
+++ WPR281 Assignment 2/script.js
@@ -0,0 +1,8 @@
+function openTicket() {
+    document.getElementById("myTicket").style.display = "block";
+  }
+  
+  function closeTicket() {
+    document.getElementById("myTicket").style.display = "none";
+  }
+  localStorage.setItem('TitleAuthor',getElementById(TicketTitle));
