c82327a176d5715583784658de8ac71ab2ec32a4
DaleWillemse <DaleWillemse000@gmail.com>
2022-08-25 17:27:08 +0200

    Drop down ticket btn works after adding a ticket

--- WPR281 Assignment 2/script.js
+++ WPR281 Assignment 2/script.js
@@ -1,3 +1,4 @@
+
 function openTicket() {
   document.getElementById("myTicket").style.display = "block";
   document.getElementById("innerBody").style.opacity = "0.5"
@@ -15,7 +16,7 @@ function clearStorage() {
 
 //Adding elements and using local storage below:
 const TicketForm = document.getElementById("TicketForm");
-const TicketContainer = document.querySelector(".ticket");
+const TicketContainer = document.querySelector(".TicketContainer");
 const inputTitle = TicketForm["TicketTitle"];
 const inputAuthor = TicketForm["TicketAuthor"];
 const inputDescription = TicketForm["TicketDescription"];
@@ -36,18 +37,19 @@ const addTicket = (TicketTitle, TicketAuthor, TicketDescription, TicketType, Tic
     TicketStatus,
     TicketDateIssued,
     TicketDateETACompleted,
-    TicketDateCompleted
+    TicketDateCompleted,
 
   });
   localStorage.setItem("Tickets", JSON.stringify(Tickets));
 
-  return { TicketTitle, TicketAuthor, TicketDescription, TicketType, TicketStatus, TicketDateIssued, TicketDateETACompleted, TicketDateCompleted };
+  return { TicketTitle, TicketAuthor, TicketDescription, TicketType, TicketStatus, TicketDateIssued, TicketDateETACompleted, TicketDateCompleted, TicketNumber };
 };
 
 const CreateTicketElement = ({ TicketTitle, TicketAuthor, TicketDescription, TicketType, TicketStatus, TicketDateIssued, TicketDateETACompleted, TicketDateCompleted }) => {
 
   //create the elements
   const tTicketDiv = document.createElement('div');
+  tTicketDiv.classList.add('ticket');
   const tTicketTitle = document.createElement('h3');
   const tTicketAuthor = document.createElement('p');
   const tTicketDescription = document.createElement('p');
@@ -57,6 +59,37 @@ const CreateTicketElement = ({ TicketTitle, TicketAuthor, TicketDescription, Tic
   const tTicketDateETACompleted = document.createElement('p');
   const tTicketDateCompleted = document.createElement('p');
   const Line = document.createElement('hr')
+
+  // Ticket button to open ticket elements
+  const tbTicketButton = document.createElement('button');
+  tbTicketButton.classList.add('ticketButton');
+
+  const tbTicket = document.createElement('div');
+  tbTicket.classList.add('Ticket');
+
+  const tbTicketNumber = document.createElement('div');
+  const pTicketNumber = document.createElement('p');
+  
+  const tbTicketHeading = document.createElement('div');
+  const pTicketHeading = document.createElement('p');
+  pTicketHeading.innerText = TicketTitle;
+
+  const tbAssingedTo = document.createElement('div');
+  const pAssingedTo = document.createElement('p');
+
+  const tbDueDate = document.createElement('div');
+  const pDueDate = document.createElement('p');
+
+  tbTicketButton.appendChild(tbTicket);
+  tbTicket.appendChild(tbTicketNumber);
+  tbTicketNumber.appendChild(pTicketNumber);
+  tbTicket.appendChild(tbTicketHeading);
+  tbTicketHeading.appendChild(pTicketHeading);
+  tbTicket.appendChild(tbAssingedTo);
+  tbAssingedTo.appendChild(pAssingedTo);
+  tbTicket.appendChild(tbDueDate);
+  tbDueDate.appendChild(pDueDate);
+
 
   //Fill the content
   tTicketTitle.innerText = "Title: " + TicketTitle;
@@ -71,6 +104,7 @@ const CreateTicketElement = ({ TicketTitle, TicketAuthor, TicketDescription, Tic
   //Add to the Domain
   //The order it is appended is the order it will be displayed in.
   tTicketDiv.append(Line, tTicketTitle, tTicketAuthor, tTicketDescription, tTicketType, tTicketStatus, tTicketDateIssued, tTicketDateETACompleted, tTicketDateCompleted, Line);
+  TicketContainer.appendChild(tbTicketButton);
   TicketContainer.appendChild(tTicketDiv);
 }
 
@@ -78,8 +112,7 @@ Tickets.forEach(CreateTicketElement);
 
 TicketForm.onsubmit = (e) => {
   e.preventDefault();
-
-
+  location.reload();
   const newTicket = addTicket(
     inputTitle.value,
     inputAuthor.value,
@@ -104,3 +137,17 @@ TicketForm.onsubmit = (e) => {
   inputDateComp.value = "";
 };
 
+// Click on the ticket to open the ticket.
+document.querySelectorAll(".ticketButton").forEach(button => {
+  button.addEventListener("click", () => {
+    const ticket = button.nextElementSibling;
+    button.classList.toggle('ticket--active');
+
+    if (button.classList.contains('ticket--active')) {
+      ticket.style.maxHeight = ticket.scrollHeight + "px";
+    }
+    else {
+      ticket.style.maxHeight = 0;
+    }
+  });
+});
