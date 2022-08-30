4dd84aa576f4a988d82863a72ad306ea14214cc1
DaleWillemse <DaleWillemse000@gmail.com>
2022-08-30 17:23:19 +0200

    added password and login requirements

--- WPR281 Assignment 2/Javascript/Login.js
+++ WPR281 Assignment 2/Javascript/Login.js
@@ -1,17 +1,14 @@
 // on submit check password and username
 
-onsubmit = function() {
+onsubmit = function () {
     var password = document.getElementById("password").value;
     var username = document.getElementById("username").value;
-    if (password == "") {
-        alert("Please enter a password");
+    if (password == "admin" && username == "admin") {
+        document.getElementById("loginForm").action = "TicketDash.html"; // CHANGE TO MAIN PAGE
+        alert("Welcome to the website");
+    }
+    else {
+        alert("Please enter a username and password");
         return false;
     }
-    if (username == "") {
-        alert("Please enter a username");
-        return false;
-    }
-    alert("Welcome to the website");
-    // form action to new page
-    document.getElementById("loginForm").action = "TicketDash.html"; // CHANGE TO MAIN PAGE
 }
