5281d3ce7264da75c3d783a55bf145633364392a
DaleWillemse <DaleWillemse000@gmail.com>
2022-08-31 19:35:03 +0200

    login workingnew styles by Wian

--- WPR281 Assignment 2/Javascript/Login.js
+++ WPR281 Assignment 2/Javascript/Login.js
@@ -4,7 +4,7 @@ onsubmit = function () {
     var password = document.getElementById("password").value;
     var username = document.getElementById("username").value;
     if (password == "admin" && username == "admin") {
-        document.getElementById("loginForm").action = "index.html"; // CHANGE TO MAIN PAGE
+        document.getElementById("loginForm").action = "index.html";
     }
     else {
         alert("Please enter a username and password");
