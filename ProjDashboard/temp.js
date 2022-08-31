9e08cc0a808372b6892c0e77f0ad65dfd6c56084
AcaJones-786 <81169643+AcaJones-786@users.noreply.github.com>
2022-08-31 16:35:59 +0200

    Update temp.js

--- ProjDashboard/temp.js
+++ ProjDashboard/temp.js
@@ -13,7 +13,7 @@ const addProject = (name, desc) => {
     return name, desc;
 };
 
-
+console.log(projects);
 
 
 const createProjectElement = ({name, desc}) => {
@@ -32,20 +32,11 @@ const createProjectElement = ({name, desc}) => {
     ticketBtn.innerText = "View Tickets";
     ticketBtn.style.backgroundColor = "lightblue";
 
-//added remove button instead of remove selector
-    const removeBtn = document.createElement("button");
-    removeBtn.classList.add("removeBtn");
-    removeBtn.style.width = "65px";
-    removeBtn.style.borderRadius = "5px";
-    removeBtn.style.border = "1px solid";
-    removeBtn.innerText = "Delete!";
-    removeBtn.style.backgroundColor = "red";
-    //removeBtn.addEventListener("click", myScript);
 
-    projectName.innerText = `Project Name: ${name}` ;
+    projectName.innerText = `Project Name: \n${name}` ;
     projectDesc.innerHTML = `Project Description: \n` + desc;
 
-    projectDiv.append(projectName, projectDesc, ticketBtn, removeBtn);
+    projectDiv.append(projectName, projectDesc, ticketBtn);
     projectContainer.appendChild(projectDiv);
 
     projectContainer.style.display = projects.length === 0 ? "none" : "";
@@ -125,15 +116,9 @@ function deleteProject(){
         }
     }
     if (isFound) {
-        for (let i = 0; i < projects.length; i++) {
-            for (let j = i + 1; j < projects.length; j++) {
-                let temp = projects[j];
-                projects[j] = projects[i];
-                projects[i] = temp;                           
-            }            
-        }
-        projects.pop();
 
+        projects.splice(index, 1);
+        
         let block = document.getElementById("tbClear");
         block.innerHTML = "";
         localStorage.clear();
@@ -149,21 +134,15 @@ function deleteProject(){
 
  var input = document.getElementById("searchInput");
  input.addEventListener("keypress", function(event) {
-    // If the user presses the "Enter" key on the keyboard
     if (event.key === "Enter") {
-      // Cancel the default action, if needed
       event.preventDefault();
-      // Trigger the button element with a click
       document.getElementById("searchBtn").click();
     }
 });
 var input = document.getElementById("deleteInput");
 input.addEventListener("keypress", function(event) {
-   // If the user presses the "Enter" key on the keyboard
    if (event.key === "Enter") {
-     // Cancel the default action, if needed
      event.preventDefault();
-     // Trigger the button element with a click
      document.getElementById("removeBtn").click();
    }
 });
